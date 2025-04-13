7a3338e189f8dcf3e0ea4e589988c0fa8c9209d9
Adler Santos <adlercoelhosantos12@gmail.com>
2025-04-13 12:16:45 -0300

    style(app): finalizando estruturação do projeto

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -5,7 +5,7 @@ import CardBalance from '@/app/components/CardBalance/CardBalance';
 export default function Home() {
     return (
         <>
-            <div className="flex  flex-col w-full lg:max-w-[720px] h-max gap-10 mx-auto mt-10 mb-10 p-5 bg-white rounded-[8px] shadow-md">
+            <div className="flex flex-col w-full lg:max-w-[720px] h-max gap-10 mx-auto mt-10 mb-10 p-5 bg-white rounded-[8px] shadow-md">
                 <CardBalance balance={1000} />
                 <FormTransaction />
             </div>
